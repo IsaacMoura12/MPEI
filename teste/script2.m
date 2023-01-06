2a96b152165bf6d7a49da9d2814cd6643e4c73eb
Miguel Gomes <mig.gom@ua.pt>
2023-01-06 19:13:26 +0000

    Feito

--- teste/script2.m
+++ teste/script2.m
@@ -53,6 +53,7 @@ function yourMovies(user,YourMoviesTable, dicFilms)
         fprintf(char(dicFilms(linha,1)))
         fprintf('\n')
     end
+    fprintf('\nPress enter to continue');
     pause; clc;
 end
 
@@ -66,9 +67,11 @@ function SuggestionsOtherUsers(Nu,MinHashValue,user,YourMoviesTable,dicFilms)
             %respetivo user
         end
     end
-
+    
     [val,SimilarId] = min(Jdistances);
-
+    Jdistances(:,SimilarId) = [];
+    [val,SimilarId2] = min(Jdistances);
+    
     suggestions = [];
     for n = 1: length(YourMoviesTable{SimilarId})
         if(~ismember(YourMoviesTable{SimilarId}(n), YourMoviesTable{user}))
@@ -79,28 +82,47 @@ function SuggestionsOtherUsers(Nu,MinHashValue,user,YourMoviesTable,dicFilms)
     if isempty(suggestions)
         fprintf('Don t exists any suggestion')
     else
-        fprintf('Film Suggestions:\n')
+        fprintf('Film Suggestions of the user most similar:\n')
         for i = 1:length(suggestions)
             fprintf(suggestions(i) + '\n');
         end
     end
+    
+    suggestions = [];
+    for n = 1: length(YourMoviesTable{SimilarId2})
+        if(~ismember(YourMoviesTable{SimilarId2}(n), YourMoviesTable{user}))
+            suggestions = [suggestions string(dicFilms(n,1))];
+        end
+    end
+
+    if isempty(suggestions)
+        fprintf('Don t exists any suggestion')
+    else
+        fprintf('Film Suggestions of the seconds user most similar:\n:\n')
+        for i = 1:length(suggestions)
+            fprintf(suggestions(i) + '\n');
+        end
+    end
+    fprintf('\nPress enter to continue');
     pause; clc;
 end
 
 function SuggestionsCategories(Nu,MinHashValue,user,YourMoviesTable,dicFilms,MinHashallCat,MinHashCat)
    
     allmovies = YourMoviesTable{user};
+    %usermov = cell2mat(allmovies);
+    Nusermovies = length(allmovies);
     
     Nc = length(dicFilms);
-    J=zeros(Nc);
+    J=zeros(Nc); 
     k=100;
 
     h=waitbar(0, 'Calculating');
-    for i = 1:length(allmovies)
-        waitbar(i/length(allmovies), h);
-        n2 = allmovies(i);
-        for n1 = 1:Nc
-            if n2 ~= n1
+    for i = 1:Nusermovies
+        waitbar(i/Nusermovies, h);
+        n1 = allmovies(i);
+        for n2 = 1:Nc
+            if n2 ~= n1 && ~ismember(n2,allmovies)
                 J(n1,n2) = sum(MinHashCat(n1,:) ~= MinHashCat(n2,:))/k;
             end
         end
@@ -110,14 +132,35 @@ function SuggestionsCategories(Nu,MinHashValue,user,YourMoviesTable,dicFilms,Min
     threshold = 0.8;
     SimilarMovies = cell(1,3);
     k = 1;
-    for n1 = 1:Nc
-        if J(n1,n2) < threshold
-            SimilarMovies(k,:) = {char(films(n1)) char(films(n2)) J(n1,n2)};
-            k = k+1;
+    
+    h=waitbar(0, 'Calculating');
+    for i = 1:Nusermovies
+        waitbar(i/Nusermovies, h);
+        n1 = allmovies(i);
+        for n2 = 1:Nc
+            if J(n1,n2) < threshold && ~ismember(n2,allmovies)
+                SimilarMovies(k,:) = {n1 n2 J(n1,n2)};
+                k = k+1;
+            end
         end
     end
 
+    
+    allsimilarMovies = cell2mat(SimilarMovies(:,2));
+    similar2Movies = mode(allsimilarMovies,'all');
 
-    clc;
+    ismember(similar2Movies, allmovies)
+    
+    fprintf('\nTwo movies most common to you: ');
+    fprintf('%s, ',char(films(similar2Movies)));
+    x = find(allsimilarMovies == similar2Movies);
+
+    allsimilarMovies(x) = [];
+    
+    similar2Movies = mode(allsimilarMovies,'all');
+    fprintf(char(films(similar2Movies)));
+
+    fprintf('\nPress enter to continue');
+    pause; clc;
 end
 
